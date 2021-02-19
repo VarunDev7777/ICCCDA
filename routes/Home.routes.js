1f055f07ec5631a0de37d6b7edc0f6c671740649
Varun Dev Sharma <dudevarun7777@gmail.com>
2021-02-19 08:48:43 +0530

    ICCCDA First Commit

---
+++ routes/Home.routes.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const Router = express.Router();
+
+const HomeControl = require("../controllers/Home.controller");
+
+Router.get("/", HomeControl.getHome);
+Router.get("/about", HomeControl.getAbout);
+Router.get("/papers", HomeControl.getPapers);
+Router.get("/special-sessions", HomeControl.getSessions);
+Router.get("/registrations", HomeControl.getRegistration);
+Router.get("/sponsorship", HomeControl.getSponsorship);
+Router.get("/publication", HomeControl.getPublication);
+Router.get("/committe", HomeControl.getCommitte);
+Router.get("/conference-venue", HomeControl.getVenue);
+Router.get("/downloads", HomeControl.getDownloads);
+Router.get("/previous-conference", HomeControl.getPreviousConference);
+
+// admin routes
+Router.get("/admin", HomeControl.getadmin);
+Router.get("/admin/committee", HomeControl.getadmincomm);
+
+// Post Routes
+Router.post(
+  "/admin/papers/call-for-paper",
+  HomeControl.postPapersCallForPapers
+);
+Router.post(
+  "/admin/committe/steering-committee",
+  HomeControl.postSteeringCommitteeMembers
+);
+Router.post(
+  "/admin/committe/advisory-committee",
+  HomeControl.postAdvisoryCommitteeMembers
+);
+Router.post(
+  "/admin/committe/technical-program-committee",
+  HomeControl.postTechinicalCommitteeMembers
+);
+Router.post(
+  "/admin/committe/european-technical-committee",
+  HomeControl.postEuropeanCommitteeMembers
+);
+
+module.exports = Router;
